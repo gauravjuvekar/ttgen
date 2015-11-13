eba904807b2e9d2c95d0c5641a130e42d59302c3
Gaurav Juvekar <gauravjuvekar@gmail.com>
2015-11-13 23:21:40 +0530

    Add message in case of completely infeasible problems

--- src/core/interface.c
+++ src/core/interface.c
@@ -25,16 +25,34 @@
 
 static void evolve_CB(GtkButton *button, CallBackData *data) {
 	(void)button;
+	Meta meta = Meta_from_db(data->db);
+	if (!meta.db_schedules_valid) {
+		delete_db_Population(data->db);
+	}
+
+	if (meta.n_time_slots * meta.n_rooms < meta.n_allocs) {
+		GtkMessageDialog *dialog = (GtkMessageDialog *)gtk_message_dialog_new(
+			(GtkWindow *)gtk_builder_get_object(data->builder, "main_window"),
+			GTK_DIALOG_DESTROY_WITH_PARENT,
+			GTK_MESSAGE_ERROR,
+			GTK_BUTTONS_CLOSE,
+			"Number of allocations[%d] is greater than "
+			"total number of slots(time slots * rooms)[%d].\n"
+			"There is no feasible solution. Reduce allocations or increase "
+			"rooms and/or time slots.",
+			meta.n_allocs, (meta.n_time_slots * meta.n_rooms));
+		gtk_dialog_run(GTK_DIALOG(dialog));
+		gtk_widget_destroy((GtkWidget *)dialog);
+		return;
+	}
+
+
 	gfloat target_fitness = gtk_spin_button_get_value(
 		(GtkSpinButton *)gtk_builder_get_object(data->builder,
 		                                        "target_fitness_spin_button"));
 	gint target_generations = gtk_spin_button_get_value_as_int(
 		(GtkSpinButton *)gtk_builder_get_object(
 			data->builder, "target_generations_spin_button"));
-	Meta meta = Meta_from_db(data->db);
-	if (!meta.db_schedules_valid) {
-		delete_db_Population(data->db);
-	}
 	reset_pks(data->db);
 	const Allocation *allocs = Allocations_from_db(data->db, &meta);
 	setup_population(meta.n_population, data->db, &meta, allocs);
